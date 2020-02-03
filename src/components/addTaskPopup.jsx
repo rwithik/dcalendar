f53f61fa8563f2a5fc20eb56c39539dc25caef0d
DILIP S <dilipskumar1410@gmail.com>
2020-02-04 03:49:30 +0530

    temo fix

--- src/components/addTaskPopup.jsx
+++ src/components/addTaskPopup.jsx
@@ -9,7 +9,7 @@ class AddTask extends Component {
     title: "",
     start: new Date(),
     end: new Date(),
-    group: JSON.parse(localStorage.getItem("calendar.groups")) || ""
+    group: localStorage.getItem("calendar.groups") || ""
   };
 
   handleChangeStartDate = d => {
